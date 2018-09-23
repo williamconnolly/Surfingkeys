b68682bc8b8464b4169487bfbdd88abe38b939f0
William Connolly <william.connolly@lithium.com>
2018-09-23 06:56:09 -0500

    Added HumbleNewtabPage compatability

--- dotsurfingkeys.js
+++ dotsurfingkeys.js
@@ -1,16 +1,20 @@
-mapall(["A", "<Ctrl-a>", "H"], "E"); // prev tab
-mapall(["D", "L", "<Ctrl-d>"], "R"); // next tab
+mapall(["A", "<Ctrl-a>", "H", ","], "E"); // prev tab
+mapall(["D", "L", "<Ctrl-d>", "."], "R"); // next tab
 mapall(["gx^", "gx6"], "gx0"); // close tabs to left. ^ is regex 'beginning of line' char and also capital 6
 
-map("<Ctrl-s>", "J"); map("<Ctrl-w>", "K"); // scroll page up and down
-map("w", "k"); map("s", "j"); 
-map("h", "a"); map("l", "d"); // history back/forward
-map("=", "z="); map("-", "z-"); // zoom in and out 
+map("<Ctrl-s>", "J");
+map("<Ctrl-w>", "K"); // scroll page up and down
+map("w", "k");
+map("s", "j");
+map("h", "a");
+map("l", "d"); // history back/forward
+map("=", "z=");
+map("-", "z-"); // zoom in and out 
 map("gx4", "gx$"); // close all tabs to the right. $ is regex 'end of line' char and capital 4
 
 
 Hints.scrollKeys = "0jk";
-settings.autoFocus = false;
+settings.autoFocus = true;
 settings.richHintsForKeystroke = 300;
 settings.omnibarMaxResults = 20;
 settings.omnibarSuggestion = true;
@@ -19,10 +23,32 @@ settings.tabThreshold = 20;
 settings.scrollStepSize = 150;
 settings.focusFirstCandidate = true;
 settings.hintThreshold = 50;
-settings.interceptedErrors = ["*"];
+settings.interceptedErrors = [];
 
-addSearchAlias('jira', 'JIRA RES', 'https://jira.dev.lithium.com/secure/QuickSearch.jspa?searchString=');
-addSearchAlias('board', 'JIRA Board', 'https://jira.dev.lithium.com/secure/RapidBoard.jspa?project=');
+/** 
+ * Utility functions
+ */
+const getNested = (p, o) => p.reduce((xs, x) => (xs && xs[x]) ? xs[x] : null, o);
+
+mapkey("gs", "#14Scroll to StackOverflow answers", function () {
+    var host = document.location.host;
+    var top = -1;
+    if (host.match(/reddit/)) {
+        top = document.querySelector(".commentarea > .sitetable").offsetTop + 200;
+    } else if (host.match(/stackoverflow/)) {
+        top = document.querySelector("#answers-header").offsetTop + 30;
+    }
+    window.scroll({
+        behavior: 'smooth',
+        top: top
+    })
+});
+
+
+var jira = 'https://jira.dev.lithium.com/';
+
+// addSearchAlias('jira', 'JIRA RES', `${jira}/secure/QuickSearch.jspa?searchString=`);
+addSearchAlias('board', 'JIRA Board', `${jira}/secure/RapidBoard.jspa?project=`);
 
 var base = "https://dev-lswcore-jenkins-master.dev.aws.lcloud.com";
 addSearchAlias("job", "Jenkins Job", `${base}/job/`, `${base}/search/suggest?query=`, function (response) {
@@ -33,6 +59,23 @@ addSearchAlias("job", "Jenkins Job", `${base}/job/`, `${base}/search/suggest?que
 
     return suggestions || [];
 });
+
+addSearchAlias("jira", "JIRA Search", `${jira}/browse/`, `${jira}/rest/quicksearch/1.0/productsearch/search?q=`, function (response) {
+    var res = JSON.parse(response.text);
+    var suggestions = getNested([0, 'items'], res) || [];
+    suggestions = suggestions.map(({
+        title,
+        subtitle,
+        url
+    }) => `${subtitle}: ${title}`);
+    console.log(suggestions);
+    return suggestions;
+});
+
+const saveKeys = ['<Ctrl-a>', '<Ctrl-d>', 'a', 'd', 't', 'x', 'w', 's', 'gg', 'G', 'i'];
+unmapAllExcept(saveKeys, /amazon\.com/i);
+unmapAllExcept(saveKeys, /youtube\.com/i);
+unmap('t', /github\.com/i);
 
 /* TODO:
 - JIRA search
